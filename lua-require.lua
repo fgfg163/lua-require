0a22412d14d43c0fa2923b3a0f9f5aedf65b8b1e
fgfg163 <fgfg163>
2018-01-04 03:41:13 +0800

    no message

--- lua-require.lua
+++ lua-require.lua
@@ -3,23 +3,33 @@ local _ENV = _ENV
 
 do
   string.split = string.split or function(str, d)
-    local lst = {}
-    local n = string.len(str) --长度
-    local start = 1
-    while start <= n do
-      local i = string.find(str, d, start) -- find 'next' 0
-      if i == nil then
-        table.insert(lst, string.sub(str, start, n))
-        break
+    if str == '' and d ~= '' then
+      return { str }
+    elseif str ~= '' and d == '' then
+      local lst = {}
+      for key = 1, string.len(str) do
+        table.insert(lst, string.sub(str, key, 1))
       end
-      table.insert(lst, string.sub(str, start, i - 1))
-      if i == n then
-        table.insert(lst, "")
-        break
+      return lst
+    else
+      local lst = {}
+      local n = string.len(str) --长度
+      local start = 1
+      while start <= n do
+        local i = string.find(str, d, start) -- find 'next' 0
+        if i == nil then
+          table.insert(lst, string.sub(str, start, n))
+          break
+        end
+        table.insert(lst, string.sub(str, start, i - 1))
+        if i == n then
+          table.insert(lst, "")
+          break
+        end
+        start = i + 1
       end
-      start = i + 1
+      return lst
     end
-    return lst
   end
 end
 
@@ -27,18 +37,43 @@ local path = (function()
   local path = {}
   path.separator = string.find(package.path, '/') and '/' or '\\'
   path.basename = function(thePath)
-    local thePathArray = string.split(thePath, path.separator)
+    thePath = string.gsub(thePath, '\\', '/')
+    local thePathArray = string.split(thePath, '/')
     local res = table.remove(thePathArray)
     return res
   end
   path.dirname = function(thePath)
-    local thePathArray = string.split(thePath, path.separator)
+    thePath = string.gsub(thePath, '\\', '/')
+    local thePathArray = string.split(thePath, '/')
     table.remove(thePathArray)
     return table.concat(thePathArray, path.separator)
   end
   path.extname = function()
   end
-  path.join = function()
+  path.join = function(...)
+    local pathArray = { ... }
+    local resultPathArray = {}
+    for key = 1, #pathArray do
+      if pathArray[key] ~= '' then
+        local thePath = string.gsub(pathArray[key], '\\', '/')
+        local thePathArray = string.split(thePath, '/')
+        for key2 = 1, #thePathArray do
+          local theName = thePathArray[key2]
+          if theName == '' and #resultPathArray > 0 then
+          elseif theName == '.' and #resultPathArray > 0 then
+          elseif theName == '..' and #resultPathArray == 1 and resultPathArray[1] == '' then
+            table.remove(resultPathArray)
+          elseif theName == '..' and #resultPathArray == 1 and resultPathArray[1] == '.' then
+            resultPathArray = { '..' }
+          elseif theName == '..' and #resultPathArray > 0 then
+            table.remove(resultPathArray)
+          else
+            table.insert(resultPathArray, theName)
+          end
+        end
+      end
+    end
+    return table.concat(resultPathArray, path.separator)
   end
   path.relative = function()
   end
@@ -46,21 +81,23 @@ local path = (function()
     local pathArray = { ... }
     local resultPathArray = {}
     for key = 1, #pathArray do
-      local thePath = string.gsub(pathArray[key], '\\', '/')
-      local thePathArray = string.split(thePath, '/')
-      for key2 = 1, #thePathArray do
-        local theName = thePathArray[key2]
-        if theName == '' and key2 == 1 then
-          resultPathArray = { '' }
-        elseif theName == '.' and #resultPathArray > 0 then
-        elseif theName == '..' and #resultPathArray == 1 and resultPathArray[1] == '' then
-          table.remove(resultPathArray)
-        elseif theName == '..' and #resultPathArray == 1 and resultPathArray[1] == '.' then
-          resultPathArray = { '..' }
-        elseif theName == '..' and #resultPathArray > 0 then
-          table.remove(resultPathArray)
-        else
-          table.insert(resultPathArray, theName)
+      if pathArray[key] ~= '' then
+        local thePath = string.gsub(string.gsub(pathArray[key], '\\', '/'), '/$', '')
+        local thePathArray = string.split(thePath, '/')
+        for key2 = 1, #thePathArray do
+          local theName = thePathArray[key2]
+          if theName == '' and key2 == 1 then
+            resultPathArray = { '' }
+          elseif theName == '.' and #resultPathArray > 0 then
+          elseif theName == '..' and #resultPathArray == 1 and resultPathArray[1] == '' then
+            table.remove(resultPathArray)
+          elseif theName == '..' and #resultPathArray == 1 and resultPathArray[1] == '.' then
+            resultPathArray = { '..' }
+          elseif theName == '..' and #resultPathArray > 0 then
+            table.remove(resultPathArray)
+          else
+            table.insert(resultPathArray, theName)
+          end
         end
       end
     end
@@ -78,29 +115,29 @@ requireFactory = function(filePath)
       error('bad argument #1 to \'require\' (string expected, got ' .. type(loadpath) .. ')', 2)
     end
 
-    local requirePath = path.resolve(path.dirname(filePath), loadpath)
-    local requirePathKey = string.gsub(requirePath, basePath, '')
+    local requirePath = path.resolve(filePath, loadpath)
+    local absolutePath = path.join(basePath, requirePath)
 
-    if not package.loaded[requirePathKey] then
-      if not package.preload[requirePathKey] then
+    if not package.loaded[requirePath] then
+      if not package.preload[requirePath] then
         local file
         local requireSource
         local res, err = pcall(function()
-          file = io.open(requirePath, 'r')
+          file = io.open(absolutePath, 'r')
           requireSource = file:read('*a')
         end)
         if not res then
-          error('file \'' .. requirePath .. '\' not exist', 2)
+          error('file \'' .. absolutePath .. '\' not exist', 2)
         end
         if file then
           file.close()
         end
         requireSource = 'local require, modePath = ...; ' .. requireSource
-        package.preload[requirePathKey] = assert(load(requireSource, '@' .. requirePath, 'bt', _ENV))
+        package.preload[requirePath] = assert(load(requireSource, '@' .. absolutePath, 'bt', _ENV))
       end
-      package.loaded[requirePathKey] = package.preload[requirePathKey](requireFactory(requirePath), requirePath) or true
+      package.loaded[requirePath] = package.preload[requirePath](requireFactory(path.dirname(requirePath)), requirePath) or true
     end
-    return package.loaded[requirePathKey]
+    return package.loaded[requirePath]
   end
 end
 
@@ -119,6 +156,6 @@ return function()
     _require = require
     local filePath = string.gsub(result.source, '^@', '')
     basePath = path.dirname(filePath)
-    require = requireFactory(filePath)
+    require = requireFactory('/')
   end
 end
