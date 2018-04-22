eed0759ab0cb14bd9501b2663695ebf4679aa672
fgfg163 <fgfg163>
2018-04-23 02:22:50 +0800

    no message

--- lua-require.lua
+++ lua-require.lua
@@ -115,29 +115,33 @@ requireFactory = function(filePath)
       error('bad argument #1 to \'require\' (string expected, got ' .. type(loadpath) .. ')', 2)
     end
 
-    local requirePath = path.resolve(filePath, loadpath)
-    local absolutePath = path.join(basePath, requirePath)
+    if string.match(loadpath, '^%.%/') or string.match(loadpath, '^%.%.%/') or string.match(loadpath, '^%/') then
+      local requirePath = path.resolve(filePath, loadpath)
+      local absolutePath = path.join(basePath, requirePath)
 
-    if not package.loaded[requirePath] then
-      if not package.preload[requirePath] then
-        local file
-        local requireSource
-        local res, err = pcall(function()
-          file = io.open(absolutePath, 'r')
-          requireSource = file:read('*a')
-        end)
-        if not res then
-          error('file \'' .. absolutePath .. '\' not exist', 2)
+      if not package.loaded[requirePath] then
+        if not package.preload[requirePath] then
+          local file
+          local requireSource
+          local res, err = pcall(function()
+            file = io.open(absolutePath, 'r')
+            requireSource = file:read('*a')
+          end)
+          if not res then
+            error('file \'' .. absolutePath .. '\' not exist', 2)
+          end
+          if file then
+            file.close()
+          end
+          requireSource = 'local require, modePath = ...; ' .. requireSource
+          package.preload[requirePath] = assert(load(requireSource, '@' .. absolutePath, 'bt', _ENV))
         end
-        if file then
-          file.close()
-        end
-        requireSource = 'local require, modePath = ...; ' .. requireSource
-        package.preload[requirePath] = assert(load(requireSource, '@' .. absolutePath, 'bt', _ENV))
+        package.loaded[requirePath] = package.preload[requirePath](requireFactory(path.dirname(requirePath)), requirePath) or true
       end
-      package.loaded[requirePath] = package.preload[requirePath](requireFactory(path.dirname(requirePath)), requirePath) or true
+      return package.loaded[requirePath]
+    else
+      return originRequire(loadpath)
     end
-    return package.loaded[requirePath]
   end
 end
 
@@ -145,7 +149,6 @@ return function()
   local result = debug.getinfo(2, 'S')
   if string.match(result.short_src, '%[string') then
     local newMain = string.gsub(result.source, '%.lua$', '')
-    package.preload[newMain] = nil
     package.loaded[newMain] = nil
     originRequire(newMain)
     os.exit()
@@ -153,9 +156,13 @@ return function()
   end
 
   if not _require then
-    _require = require
+    _require = originRequire
     local filePath = string.gsub(result.source, '^@', '')
     basePath = path.dirname(filePath)
     require = requireFactory('/')
   end
+
+  return {
+    basePath = basePath,
+  }
 end
